6e6055b8b667ed2e9728421999be2709f49a1cd1
Talles L <talleslasmar@gmail.com>
2015-01-19 18:06:23 -0200

    refactoring

--- bin/qckwinsvc.js
+++ bin/qckwinsvc.js
@@ -1,103 +1,88 @@
 #! /usr/bin/env node
 
-var prompt = require('prompt')
-var cli = require('optimist')
-  .usage('Usage: $0\n       $0 --uninstall')
-  .describe('name', 'Windows service name to install/uninstall (don\'t confuse with the display name')
-  .describe('description', 'Service description')
-  .describe('script', 'Path of the node script to be installed as a service')
-  .describe('startImmediately', 'Should the service get started immediately')
-var Service = require('node-windows').Service
+var cli     = require('optimist')
+  , prompt  = require('prompt')
+  , Service = require('node-windows').Service
 
-var argv = cli.argv
+setupCli()
 
-function showHelp () {
-  cli.showHelp()
-  process.exit()
+if (cli.argv.h || cli.argv.help) help()
+else if (cli.argv.uninstall) uninstall()
+else install()
+
+function setupCli () {
+  cli.usage('Usage: $0\n       $0 --uninstall')
+     .describe('name',             'Windows service name to install/uninstall (don\'t confuse with the display name.')
+     .describe('description',      'Service description.')
+     .describe('script',           'Path of the node script to be installed as a service.')
+     .describe('startImmediately', 'Should the service get started immediately.')
 }
 
-function read(/* [propertyNames], callback */) {
-  var properties = {
-    'name': {
-      description: 'Service name',
-      required: true,
-      pattern: /^[a-zA-Z\-]+$/,
-      message: 'Name must be only letters or dashes'
-    },
-    'description': {
-      description: 'Service description'
-    },
-    'script': {
-      description: 'Node script path',
-      required: true
-    },
-    'startImmediately': {
-      description: 'Should the service get started immediately? (y/n)',
-      before: function(value) {
-        var bool = ['yes', 'y'].indexOf(value.toLowerCase()) != -1
-        return bool;
+function help () {
+  cli.showHelp()
+}
+
+function install () {
+  readArgs(promptOptions(), run)
+}
+
+function uninstall () {
+  var options = promptOptions()
+  delete options.properties.description
+  delete options.properties.startImmediately
+  readArgs(options, run)
+}
+
+function promptOptions () {
+  return {
+    properties: {
+      name: {
+        description: 'Service name',
+        required:    true,
+        pattern:     /^[a-zA-Z\-]+$/,
+        message:     'Name must be only letters or dashes'
+      },
+      description: {
+        description: 'Service description'
+      },
+      script: {
+        description: 'Node script path',
+        required:    true
+      },
+      startImmediately: {
+        description: 'Should the service get started immediately? (y/n)',
+        before: function (value) {
+          value = value.toLowerCase()
+          return value === 'yes' || value === 'y'
+        }
       }
     }
-  };
-
-  // default to read all and expect callback as first param
-  var toRead = properties;
-  var callback = arguments[0];
-
-  if (callback instanceof Array) {
-    // when first param is an array of property names
-    toRead = callback.reduce(function(map, propertyName) {
-      map[propertyName] = properties[propertyName];
-      return map;
-    }, {});
-
-    callback = arguments[1];
   }
-
-  // in case user specifies data in cli
-  prompt.override = argv;
-
-  prompt.get({
-    properties: toRead
-  }, callback);
-};
-
-function bindEvents (service, properties) {
-  service.on('install', function () {
-    console.log('Service installed')
-
-    if (properties.startImmediately) { service.start() }
-  })
-  service.on('alreadyInstalled', function () { console.log('Service already installed') })
-  service.on('invalidInstallation', function () { console.log('Invalid service installation (installation is detected but required files are missing)') })
-  service.on('uninstall', function () { console.log('Service uninstalled') })
-  service.on('start', function () { console.log('Service started') })
-  service.on('stop', function () { console.log('Service stopped') })
-  service.on('error', function () { console.log('An error occurred') })
 }
 
-/*
- * RUN
- */
-
-if (argv.h || argv.help) showHelp();
-
-var readArgs = [
-  function run(error, properties) {
-    if (error) {
-      throw error;
-    }
-
-    var service = new Service(properties);
-
-    bindEvents(service, properties);
-
-    service[argv.uninstall ? 'uninstall' : 'install']();
-  }
-];
-
-if (argv.uninstall) {
-  readArgs.unshift(['name', 'script']);
+function readArgs (options, callback) {
+  prompt.override = cli.argv
+  prompt.get(options, callback)
 }
 
-read.apply(this, readArgs);
+function run (err, options) {
+  if (err) throw err
+
+  var service = new Service(options)
+  bindEvents(service, options)
+  service[cli.argv.uninstall ? 'uninstall' : 'install']()
+}
+
+function bindEvents (service, options) {
+  service.on('alreadyInstalled',    function () { console.log('Service already installed.') })
+  service.on('invalidInstallation', function () { console.log('Invalid service installation (installation is detected but required files are missing).') })
+  service.on('uninstall',           function () { console.log('Service uninstalled.') })
+  service.on('start',               function () { console.log('Service started.') })
+  service.on('stop',                function () { console.log('Service stopped.') })
+  service.on('error',               function () { console.log('An error occurred.') })
+  service.on('install',             function () {
+                                      console.log('Service installed.')
+                                      if (options.startImmediately) service.start()
+                                    })
+}
+
